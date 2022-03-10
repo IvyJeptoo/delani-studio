8380e71fee2d98d391eca0845a199768cecc1b96
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-03-10 16:24:46 +0300

    add toggle functionality

---
+++ javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".img1").click(function(){
+        $(".pdesign").toggle();
+        $(".design").toggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".img2").click(function(){
+        $(".pdev").toggle();
+        $(".dev").toggle();
+    })
+})
+$(document).ready(function(){
+    $(".img3").click(function(){
+        $(".pproduct").toggle();
+        $(".product").toggle();
+    })
+})
+
